7b86535b1b5089c60c9ee4c364c4df0fdad5723d
zhaochao <zhaochao@syberos.com>
2018-10-11 18:46:42 +0800

    niubi

--- mail/src/main/java/sendMail/App.java
+++ mail/src/main/java/sendMail/App.java
@@ -2,12 +2,21 @@ package sendMail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App   {
     public static void main(String args[]){
         SpringApplication.run(App.class, args);
 
 
+    }
+
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+
+        return application.sources(App.class);
     }
 }
